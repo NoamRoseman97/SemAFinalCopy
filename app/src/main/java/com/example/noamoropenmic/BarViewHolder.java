0ff2fcc5248d72057f9108e9041713a3b22245f3
NoamRoseman97 <Noam.roseman@gmail.com>
2023-03-19 17:14:32 +0200

    Duplicate test - remove

--- app/src/main/java/com/example/noamoropenmic/BarViewHolder.java
+++ app/src/main/java/com/example/noamoropenmic/BarViewHolder.java
@@ -26,10 +26,5 @@ public class BarViewHolder extends RecyclerView.ViewHolder {
         address = itemView.findViewById(R.id.address);
 
 
-        cardView = itemView.findViewById(R.id.cardview);
-        barImage = itemView.findViewById(R.id.barImage);
-        barName = itemView.findViewById(R.id.barName);
-        phone = itemView.findViewById(R.id.phone);
-        address = itemView.findViewById(R.id.address);
     }
 }
