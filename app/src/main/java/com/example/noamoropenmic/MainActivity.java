7324e6bb0308c81a7376b1cced8b884261768278
NoamRoseman97 <Noam.roseman@gmail.com>
2023-08-02 12:47:45 +0300

    Commit 25.3.2023 15:42 (New BarTransition button)

--- app/src/main/java/com/example/noamoropenmic/MainActivity.java
+++ app/src/main/java/com/example/noamoropenmic/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         Button btn1 = findViewById(R.id.AUbtn);
         btn1.setOnClickListener(new View.OnClickListener() {
